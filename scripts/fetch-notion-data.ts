1c34beda38563a4e165c271f0efa2ee28e437672
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-03-21 03:43:58 -0500

    notion data: fix* for linked databases

--- scripts/fetch-notion-data.ts
+++ scripts/fetch-notion-data.ts
@@ -182,35 +182,45 @@ async function formatBlock(block: BlockObjectResponse): Promise<string> {
 	if (block.type === "child_database") {
 		console.info("Querying database:", block.child_database.title)
 
-		const database = await notion.databases.retrieve({ database_id: block.id })
-		invariant(
-			isFullDatabase(database),
-			`expected full database: ${prettify(database)}`,
-		)
+		try {
+			const database = await notion.databases.retrieve({
+				database_id: block.id,
+			})
+			invariant(
+				isFullDatabase(database),
+				`expected full database: ${prettify(database)}`,
+			)
 
-		const columnNames = sortBy(Object.values(database.properties), [
-			(item) => (item.type === "title" ? 0 : 1),
-		]).map((item) => item.name)
+			const columnNames = sortBy(Object.values(database.properties), [
+				(item) => (item.type === "title" ? 0 : 1),
+			]).map((item) => item.name)
 
-		const rows = []
+			const rows = []
 
-		for await (const item of iteratePaginatedAPI(notion.databases.query, {
-			database_id: block.id,
-		})) {
-			if (isFullPage(item)) {
-				rows.push(await flattenDatabaseRow(item))
-			} else {
-				console.warn(`Unsupported database item:`, item)
+			for await (const item of iteratePaginatedAPI(notion.databases.query, {
+				database_id: block.id,
+			})) {
+				if (isFullPage(item)) {
+					rows.push(await flattenDatabaseRow(item))
+				} else {
+					console.warn(`Unsupported database item:`, item)
+				}
 			}
-		}
 
-		return compactJoin("\n", [
-			`| ${columnNames.join(" | ")} |`,
-			`| ${columnNames.map(() => "---").join(" | ")} |`,
-			...rows.map(
-				(row) => `| ${columnNames.map((key) => row[key]).join(" | ")} |`,
-			),
-		])
+			return compactJoin("\n", [
+				`| ${columnNames.join(" | ")} |`,
+				`| ${columnNames.map(() => "---").join(" | ")} |`,
+				...rows.map(
+					(row) => `| ${columnNames.map((key) => row[key]).join(" | ")} |`,
+				),
+			])
+		} catch (error) {
+			// notion errors when trying to fetch a linked database,
+			// but we also can't tell a linked database from a regular database,
+			// so we'll just try/catch
+			console.warn(`Failed to query database:`, error)
+			return `<!-- failed to query database: ${error} -->`
+		}
 	}
 
 	console.warn(`Unsupported block type ${block.type}, skipping`)
